0c47b36c602b9c07af1a8e61ce0935a1356d5031
arisaidalvarez <arisaidalvarez@gmail.com>
2021-10-21 16:50:23 -0500

    Update file.php

--- file.php
+++ file.php
@@ -1,1 +1,10 @@
-<?php echo "Hello I'm a php file"; ?>
+<!DOCTYPE html>
+<html lang="en" dir="ltr">
+  <head>
+    <meta charset="utf-8">
+    <title>Arisaid Alvarez Medina</title>
+  </head>
+  <body>
+    <?php echo "Hello I'm a php file"; ?>
+  </body>
+</html>
